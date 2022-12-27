8b3da4fa8a664dbf932ffc9b83bab21781abb2de
zanetazuranska <zanetazuranska444@gmail.com>
2022-12-27 13:06:57 +0100

    Heavy

---
+++ Assets/_Game/Scripts/Movement/Movement1.cs
@@ -0,0 +1,217 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
+public class Movement1 : MonoBehaviour
+{
+    //For new states use next powers of 2
+    public enum MovementState
+    {
+        Standing = 1,
+        Crouching = 2,
+        Creeping = 4,
+        Walking = 8,
+        Running = 16,
+        Jumping = 32,
+    }
+
+    private PlayerInput _playerInput;
+    private int _movementState = (int)MovementState.Standing; //State of the player, represented ad bits 
+    private Rigidbody2D _rigidbody;
+    private bool _onGround = true;
+
+    [Header("Movement")]
+    [Range(0f, 10f)]
+    [SerializeField] private float _baseMovementSpeed;
+    private const float MOVEMENTSPEED_SCALE = 100f;
+
+    [Range(0f, 10f)]
+    [SerializeField] private float _crouchMultipier;
+
+    [Range(0f, 10f)]
+    [SerializeField] private float _runMultiplier;
+
+    [Range(0f, 10f)]
+    [SerializeField] private float _creepMultipier;
+
+    [Range(0f, 10f)]
+    [SerializeField] private float _jumpForce;
+    private const float JUMPFORCE_SCALE = 10000f;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        //Init player input
+        _playerInput = new PlayerInput();
+        _playerInput.InGame.Enable();
+        _playerInput.InGame.Walk.performed += OnWalkPerformed;
+        _playerInput.InGame.Walk.canceled += OnWalkCanceled;
+        _playerInput.InGame.Run.performed += OnRunPerformed;
+        _playerInput.InGame.Run.canceled += OnRunCanceled;
+        _playerInput.InGame.Creep.performed += OnCreepPerformed;
+        _playerInput.InGame.Creep.canceled += OnCreepCanceled;
+        _playerInput.InGame.Jump.performed += OnJump;
+        _playerInput.InGame.Crouch.performed += OnCrouchPerformed;
+        _playerInput.InGame.Crouch.canceled += OnCrouchCanceled;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        //Calculate movement speed
+        //Left - Right movment, read every frame
+        float moveRaw = _playerInput.InGame.Walk.ReadValue<float>() * _baseMovementSpeed * MOVEMENTSPEED_SCALE;
+
+        List<MovementState> currentStates = GetMovementStates2();
+
+        //Modify movement speed only when player is on the ground
+        if (_onGround)
+        {
+            if (currentStates.Contains(MovementState.Running))
+            {
+                //Run => unmset creeping and crouching
+                _movementState &= ~((int)MovementState.Creeping | (int)MovementState.Crouching);
+
+                //Force relese keys
+                _playerInput.InGame.Creep.Reset();
+                _playerInput.InGame.Crouch.Reset();
+
+                moveRaw *= _runMultiplier;
+            }
+            else if (currentStates.Contains(MovementState.Creeping))
+            {
+                //Creep => unset crouching and running
+                _movementState &= ~((int)MovementState.Crouching | (int)MovementState.Running);
+
+                //Force relese keys
+                _playerInput.InGame.Run.Reset();
+                _playerInput.InGame.Crouch.Reset();
+
+                moveRaw *= _creepMultipier;
+            }
+            else if (currentStates.Contains(MovementState.Crouching))
+            {
+                //Crouching => unsert creeping and running
+                _movementState &= ~((int)MovementState.Creeping | (int)MovementState.Running);
+
+                //Force relese keys
+                _playerInput.InGame.Creep.Reset();
+                _playerInput.InGame.Run.Reset();
+
+                moveRaw *= _crouchMultipier;
+            }
+        }
+
+        //Apply movement speed
+        _rigidbody.velocity = new Vector2(moveRaw * Time.fixedDeltaTime, _rigidbody.velocity.y);
+    }
+
+    private void OnWalkPerformed(InputAction.CallbackContext context)
+    {
+        //Set walking state
+        _movementState |= (int)MovementState.Walking;
+    }
+
+    private void OnWalkCanceled(InputAction.CallbackContext context)
+    {
+        //Remove walking state
+        _movementState &= ~(int)MovementState.Walking;
+    }
+
+    private void OnRunPerformed(InputAction.CallbackContext context)
+    {
+        //Set running state
+        _movementState |= (int)MovementState.Running;
+
+        //Remove creeping state
+        _movementState &= ~(int)MovementState.Creeping;
+    }
+
+    private void OnRunCanceled(InputAction.CallbackContext context)
+    {
+        //Remove running state
+        _movementState &= ~(int)MovementState.Running;
+    }
+
+    private void OnCreepPerformed(InputAction.CallbackContext context)
+    {
+        //Set creeping state
+        _movementState |= (int)MovementState.Creeping;
+
+        //Remove running state
+        _movementState &= ~(int)MovementState.Running;
+    }
+
+    private void OnCreepCanceled(InputAction.CallbackContext context)
+    {
+        //Remove creeping state
+        _movementState &= ~(int)MovementState.Creeping;
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        if (!_onGround) return;
+
+        _rigidbody.AddForce(Vector2.up * _jumpForce * Time.fixedDeltaTime * JUMPFORCE_SCALE, ForceMode2D.Impulse);
+    }
+
+    private void OnCrouchPerformed(InputAction.CallbackContext context)
+    {
+        //Set crouching state
+        _movementState |= (int)MovementState.Crouching;
+        
+    }
+
+    private void OnCrouchCanceled(InputAction.CallbackContext context)
+    {
+        //Remove crouching state
+        _movementState &= ~(int)MovementState.Crouching;
+        
+    }
+
+    //Collisions with ground objects to reset jump ability
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Ground")) return;
+        if (!GetMovementStates2().Contains(MovementState.Jumping)) return;
+        
+        //Set jumping state
+        _movementState &= ~(int)MovementState.Jumping;
+        _onGround = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Ground")) return;
+        if (GetMovementStates2().Contains(MovementState.Jumping)) return;
+
+        //Remove jumping state
+        _movementState |= (int)MovementState.Jumping;
+        _onGround = false;
+    }
+
+    /// <summary>
+    /// Get all active movement states
+    /// </summary>
+    /// <returns>Returns list of all movement states that are currently active</returns>
+    public List<MovementState> GetMovementStates2()
+    {
+        List<MovementState> states = new List<MovementState>();
+
+        //Loop throgh all MovementStates
+        for(int i = 1; i < (int)Mathf.Pow(2, Enum.GetNames(typeof(MovementState)).Length); i <<= 1)
+        {
+            //If MovementState flag is set, add it to list
+            if ((_movementState & i) == i) states.Add((MovementState)i);
+        }
+
+        return states;
+    }
+}
