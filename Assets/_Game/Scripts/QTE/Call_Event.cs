2574f3b65708f4f13fd0eee32afb19d4d36e305d
zanetazuranska <95565830+zanetazuranska@users.noreply.github.com>
2023-03-03 21:41:00 +0100

    Singleton and event

--- Assets/_Game/Scripts/QTE/Call_Event.cs
+++ Assets/_Game/Scripts/QTE/Call_Event.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using System;
 
 public class Call_Event : MonoBehaviour
 {
     [SerializeField] GameObject _square;
-    [SerializeField] private UnityEvent _qte;
-    [SerializeField] private UnityEvent _resetVariables;
+
+    //Check Variables
+    private QTEManager.Caller _caller;
+    private bool _oneCall = false;
+    private bool _isSuccess;
+
+    
+
 
     // Update is called once per frame
     void Update()
@@ -16,20 +23,34 @@ public class Call_Event : MonoBehaviour
         {
             if(_square.active==false)
             {
-                _resetVariables?.Invoke();
+
                 _square.SetActive(true);
+                QTEManager.Instance.QTEStart(QTEManager.Caller.Crafting, 3);
+
             }
             else
             {
                 _square.SetActive(false);
+
             }
         }
 
-        if(_square.active==true)
+        if (_square.active == true)
         {
-            _qte?.Invoke();
+            _caller=QTEManager.Instance.QTEAction(QTEManager.Caller.Crafting, 1, _isSuccess);
+        }
+
+
+    }
+
+    void Result()
+    {
+        if (_caller==QTEManager.Caller.Crafting && _isSuccess)
+        {
+            Debug.Log("win");
         }
     }
+
 
 
 }
---
+++ Assets/_Game/Scripts/QTE/QTEManager.cs
@@ -0,0 +1,279 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class QTEManager : MonoBehaviour
+{
+    public static QTEManager Instance { get; private set; }
+
+    [Header("Letter objects")]
+    [SerializeField] private GameObject j;
+    [SerializeField] private GameObject k;
+    [SerializeField] private GameObject l;
+    [SerializeField] private GameObject m;
+    [SerializeField] private GameObject o;
+    [SerializeField] private GameObject p;
+    [SerializeField] private GameObject t;
+    [SerializeField] private GameObject u;
+
+
+    [Header("QTE Settings")]
+    [SerializeField] [Range(0f, 1f)] private float _frameChange = 0.1f;
+    [SerializeField] [Range(0f, 1f)] private float _timeThreshold = 0.05f;
+
+    //Enum
+    public enum Caller
+    {
+        Crafting=1,
+    }
+
+
+    //QTE Variables
+    private float _fillAmount = 0;
+    private float _timeThresholdV = 0;
+    private bool _eventSuccess = true;
+    private GameObject[] _array;
+    private int _lottery = 0;
+    public System.Random generator = new System.Random();
+    [SerializeField] private int _howManyLettersV = 0;
+    [SerializeField] private float _timeForQTECompleted;
+
+    //Action
+    private Action _action = null;
+
+    //Events
+    public event Action<int> _qte;
+    public event Action<Caller, bool> _qteResult = null;
+    public event Action<float> _countingDown = null;
+
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    void Start()
+    {
+        _array = new GameObject[] { j, k, l, m, o, p, t, u };
+
+    }
+
+
+
+    //Update QTE state by calling QteManager(int howManyLetters)
+    public Caller QTEAction(Caller caller, int howManyLetters, bool isSuccess )
+    {
+        _qte?.Invoke(howManyLetters);
+
+        _countingDown?.Invoke(_timeForQTECompleted);
+
+        _qteResult?.Invoke(caller, isSuccess);
+
+
+        return caller;
+    }
+
+
+    //Checks whether QTE is finished or not and calls the appropriate action
+    public void QteManager(int howManyLetters)
+    {
+        if (_howManyLettersV < howManyLetters)
+        {
+            PrepareAction(EventSuccess, QTE);
+            _action?.Invoke();
+        }
+        if(_howManyLettersV == howManyLetters)
+        {
+            _qteResult += QTESuccess;
+        }
+
+        
+    }
+
+    //Subscribes to the event and resets the variables
+    public void QTEStart( Caller caller, float timeForQTECompleted)
+    {
+        _qte += QteManager;
+        _fillAmount = 0;
+        _timeThresholdV = 0;
+        _howManyLettersV = 0;
+        _eventSuccess = true;
+
+        _timeForQTECompleted = timeForQTECompleted;
+        _countingDown += CountingDown;
+    }
+
+    private void EventSuccess()
+    {
+        _eventSuccess = false;
+        _lottery = generator.Next(8);
+        _array[_lottery].SetActive(true);
+    }
+
+
+    //The essence of QTE action
+    private void QTE()
+    {
+        SwitchLetter();
+
+        _timeThresholdV += Time.deltaTime;
+
+        if (_timeThresholdV > _timeThreshold)
+        {
+            _timeThresholdV = 0;
+            _fillAmount -= .02f;
+        }
+
+        if (_fillAmount < 0)
+        {
+            _fillAmount = 0;
+        }
+
+        if (_fillAmount >= 1 && _eventSuccess == false)
+        {
+            _eventSuccess = true;
+            _array[_lottery].SetActive(false);
+            _fillAmount = 0;
+            _howManyLettersV++;
+        }
+
+        _array[_lottery].transform.GetChild(0).gameObject.GetComponent<Image>().fillAmount = _fillAmount;
+    }
+
+    private void PrepareAction(Action eventSuccess, Action qte)
+    {
+        if (_eventSuccess)
+        {
+            _action = eventSuccess;
+
+        }
+        else
+        {
+            _action = qte;
+        }
+    }
+
+    private void SwitchLetter()
+    {
+        switch (_lottery)
+        {
+            case 0:
+                {
+                    if (Input.GetKeyDown("j"))
+                    {
+                        _fillAmount += _frameChange;
+                    }
+                    break;
+                }
+            case 1:
+                {
+                    if (Input.GetKeyDown("k"))
+                    {
+                        _fillAmount += _frameChange;
+                    }
+                    break;
+                }
+            case 2:
+                {
+                    if (Input.GetKeyDown("l"))
+                    {
+                        _fillAmount += _frameChange;
+                    }
+                    break;
+                }
+            case 3:
+                {
+                    if (Input.GetKeyDown("m"))
+                    {
+                        _fillAmount += _frameChange;
+                    }
+                    break;
+                }
+            case 4:
+                {
+                    if (Input.GetKeyDown("o"))
+                    {
+                        _fillAmount += _frameChange;
+                    }
+                    break;
+                }
+            case 5:
+                {
+                    if (Input.GetKeyDown("p"))
+                    {
+                        _fillAmount += _frameChange;
+                    }
+                    break;
+                }
+            case 6:
+                {
+                    if (Input.GetKeyDown("t"))
+                    {
+                        _fillAmount += _frameChange;
+                    }
+                    break;
+                }
+            case 7:
+                {
+                    if (Input.GetKeyDown("u"))
+                    {
+                        _fillAmount += _frameChange;
+                    }
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
+    }
+
+    private void CountingDown(float timeForQTECompleted)
+    {
+        if (_timeForQTECompleted > 0)
+        {
+            _timeForQTECompleted -= Time.deltaTime;
+        }
+        else
+        {
+            _qteResult += QTEFail;
+        }
+    }
+
+    private void QTEFail(Caller caller, bool isSuccess)
+    {
+        _qte -= QteManager;
+        _qteResult -= QTEFail;
+        _countingDown -= CountingDown;
+        isSuccess = false;
+
+        _array[_lottery].SetActive(false);
+        Debug.Log("fail");
+    }
+
+    private void QTESuccess(Caller caller, bool isSuccess)
+    {
+        _qte -= QteManager;
+        _qteResult -= QTESuccess;
+        _countingDown -= CountingDown;
+        isSuccess = true;
+
+        _array[_lottery].SetActive(false);
+       Debug.Log("win");
+    }
+
+
+
+
+
+}
