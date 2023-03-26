b864cad13d346b370f95c359c6ab921f012610db
zanetazuranska <95565830+zanetazuranska@users.noreply.github.com>
2023-03-26 17:59:57 +0200

    Fix2

---
+++ Assets/_Game/Scripts/Combat System/WeaponSwitching.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class WeaponSwitching : MonoBehaviour
+{
+    [SerializeField] private int _selectedWeapon=0;
+    private int _previousSelectedWeapon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectWeapon();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _previousSelectedWeapon = _selectedWeapon;
+
+        if (Input.GetAxis("Mouse ScrollWheel")>0f)
+        {
+            if (_selectedWeapon >= transform.childCount - 1)
+                _selectedWeapon = 0;
+            else
+                _selectedWeapon++;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (_selectedWeapon <=0)
+                _selectedWeapon = transform.childCount - 1;
+            else
+                _selectedWeapon--;
+        }
+
+        if(_previousSelectedWeapon!=_selectedWeapon)
+        {
+            SelectWeapon();
+        }
+    }
+
+    private void SelectWeapon()
+    {
+        int i = 0;
+
+        foreach (Transform weapon in transform)
+        {
+            if (i == _selectedWeapon)
+
+                weapon.gameObject.SetActive(true);
+            else
+                weapon.gameObject.SetActive(false);
+
+            i++;
+        }
+    }
+
+}
