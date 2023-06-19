b20c1bf0189ece16b4880eefed59be899ba01caa
zanetazuranska <95565830+zanetazuranska@users.noreply.github.com>
2023-06-19 20:14:06 +0200

    Combination with hands

--- Assets/_Game/Scripts/Combat System/Shooting/Shooting.cs
+++ Assets/_Game/Scripts/Combat System/Shooting/Shooting.cs
@@ -6,22 +6,47 @@ using System.Linq;
 
 public class Shooting : MonoBehaviour
 {
-    [SerializeField] private Movement _movement;
-    [SerializeField] Transform _firePoint;
-    [SerializeField] WeaponSwitching _weapon;
     [SerializeField] GameObject _bulletPrefab;
+    [SerializeField] private List<Sprite> _playerHandsSpritesList;
+    private Dictionary<int, Sprite> _playerHandsSpritesDictiorary = new Dictionary<int, Sprite>();
+
+    private Movement _movement;
+    private Transform _firePoint;
+    private WeaponSwitching _weapon;
     private PlayerInput _playerInput;
+    [SerializeField] private Vector2 mousePos;
 
     private bool _canShoot=true;
 
     private void Awake()
     {
+        //Activating player input for shooting
         _playerInput = new PlayerInput();
         _playerInput.Shooting.Enable();
         _playerInput.Shooting.Shoot.performed += Shoot;
 
+        //New instance for checking the game state
         if (GameStateManager.Instance != null)
             GameStateManager.Instance.OnGameStateChanged += OnGameStateChanged;
+
+        //Downloading the appropriate components
+        _movement = GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>();
+        _firePoint = GameObject.FindGameObjectWithTag("FirePoint").GetComponent<Transform>();
+        _weapon = GameObject.FindGameObjectWithTag("WeaponHolder").GetComponent<WeaponSwitching>();
+
+        
+        //Dictionary Initialization 
+        int i = 1;
+        foreach (Sprite _sprite in _playerHandsSpritesList)
+        {
+            _playerHandsSpritesDictiorary.Add(i, _sprite);
+            i++;
+        }
+    }
+
+    private void Update()
+    {
+        mousePos = _playerInput.Shooting.MousePosition.ReadValue<Vector2>();
     }
 
     private void OnGameStateChanged(GameStateManager.GameState newGameState)
@@ -31,7 +56,6 @@ public class Shooting : MonoBehaviour
 
     private void Shoot (InputAction.CallbackContext context)
     {
-        Vector2 mousePos = _playerInput.Shooting.MousePosition.ReadValue<Vector2>();
         RaycastHit2D[] hits = Physics2D.RaycastAll(FindObjectOfType<Camera>().ScreenToWorldPoint(mousePos), Vector2.zero);
 
         if (hits.Any(x => x.collider.GetComponent<IInteractable>() != null) == false && _canShoot==true && _weapon.GetWeapon()==1 && !_movement.GetMovementStates().Contains(Movement.MovementState.Crouching) && !_movement.GetMovementStates().Contains(Movement.MovementState.Running)) 
