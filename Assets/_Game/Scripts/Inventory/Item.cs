f624106e52732b7265d131556af7620aa8077df1
zanetazuranska <95565830+zanetazuranska@users.noreply.github.com>
2023-07-28 16:04:48 +0200

    Items

--- Assets/_Game/Scripts/Inventory/Item.cs
+++ Assets/_Game/Scripts/Inventory/Item.cs
@@ -12,6 +12,14 @@ public class Item
         None = 0,
         Knife = 1,
         Gun = 2,
+        Ammo = 3,
+        Drool1 = 4,
+        Drool2 = 5,
+        GunPowder = 6,
+        Herb = 7,
+        Note = 8,
+        Vile1 = 9,
+        Vile2 = 10,
     }
 
     public ItemType itemType;
